c8511c96cc7330011124e5b1b92fa6e3c960e421
Denis <dangluva@gmail.com>
2023-06-18 18:05:05 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    # filepath = 'todos.txt'
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print('Hello from functions')
+    print(get_todos())
